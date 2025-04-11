5f0bd6d94d0bb431b2652bbcb15910627e970fdf
Ranjeet Vishwakarma <52901207+Ranjeet7875@users.noreply.github.com>
2025-04-11 20:56:24 +0530

    profilepages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
 import './App.css'
 import Landing1 from './Pages/Landing1'
+import ProfilePage from './Pages/ProfilesPages'
 
 function App() {
   return (
     <>
-      <Landing1/>
+      <ProfilePage/>
      
     </>
   )
